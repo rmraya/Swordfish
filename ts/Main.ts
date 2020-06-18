e14aa32dca0276b2b53a569fafa4a232e9e5c3e7
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2020-06-18 19:15:07 -0300

    Refactored tabs

--- ts/Main.ts
+++ ts/Main.ts
@@ -17,65 +17,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *****************************************************************************/
 
-
-class Tab {
-
-    id: string;
-    label: HTMLAnchorElement;
-    labelDiv: HTMLDivElement;
-    container: HTMLDivElement;
-
-    constructor(tabId: string, description: string, closeable: boolean) {
-        this.id = tabId;
-        this.labelDiv = document.createElement('div');
-        this.labelDiv.classList.add('tab');
-
-        this.label = document.createElement('a');
-        this.label.id = this.id;
-        this.label.innerText = description;
-        this.label.addEventListener('click', () => {
-            Main.selectTab(this.id);
-        });
-        this.labelDiv.appendChild(this.label);
-        if (closeable) {
-            let closeAnchor: HTMLAnchorElement = document.createElement('a');
-            closeAnchor.className = 'tooltip';
-            closeAnchor.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path style="stroke-width: 1.4;" d="M4 4 L12 12 M4 12 L12 4"/></svg>' +
-            '<span  class="tooltiptext bottomTooltip">Close</span>';
-            closeAnchor.style.marginLeft = '10px';
-            closeAnchor.addEventListener('click', () => {
-                Main.closeTab(this.id);
-            });
-            this.labelDiv.appendChild(closeAnchor);
-        }
-        this.container = document.createElement('div');
-        this.container.classList.add('hidden');
-    }
-
-    getId(): string {
-        return this.id;
-    }
-
-    getLabel(): HTMLDivElement {
-        return this.labelDiv;
-    }
-
-    getContainer(): HTMLDivElement {
-        return this.container;
-    }
-
-    setContainer(div: HTMLDivElement): void {
-        this.container = div;
-    }
-}
-
 class Main {
 
     electron = require('electron');
 
-    static labels: Map<String, HTMLDivElement>;
-    static tabs: Map<string, HTMLDivElement>;
-    static tabHolder: HTMLDivElement;
+    static tabHolder: TabHolder;
     static main: HTMLDivElement;
 
     projectsView: ProjectsView;
@@ -83,34 +29,25 @@ class Main {
     glossariesView: GlossariesView;
 
     constructor() {
-        Main.labels = new Map<String, HTMLDivElement>();
-        Main.tabs = new Map<string, HTMLDivElement>();
 
-        Main.tabHolder = document.getElementById('tabs') as HTMLDivElement;
+        let container: HTMLDivElement = document.getElementById('mainContainer') as HTMLDivElement;
+        Main.tabHolder = new TabHolder(container, 'main');
+
         Main.main = document.getElementById('main') as HTMLDivElement;
 
         let projectsTab = new Tab('projects', 'Projects', false);
         this.buildProjectsTab(projectsTab.getContainer());
-        Main.tabHolder.appendChild(projectsTab.getLabel());
-        Main.labels.set(projectsTab.getId(), projectsTab.getLabel());
-        Main.main.appendChild(projectsTab.getContainer());
-        Main.tabs.set(projectsTab.getId(), projectsTab.getContainer());
+        Main.tabHolder.addTab(projectsTab);
 
         let memoriesTab = new Tab('memories', 'Memories', false);
         this.buildMemoriesTab(memoriesTab.getContainer());
-        Main.tabHolder.appendChild(memoriesTab.getLabel());
-        Main.labels.set(memoriesTab.getId(), memoriesTab.getLabel());
-        Main.main.appendChild(memoriesTab.getContainer());
-        Main.tabs.set(memoriesTab.getId(), memoriesTab.getContainer());
+        Main.tabHolder.addTab(memoriesTab);
 
         let glossariesTab = new Tab('glossaries', 'Glossaries', false);
         this.buildGlossariesTab(glossariesTab.getContainer());
-        Main.tabHolder.appendChild(glossariesTab.getLabel());
-        Main.labels.set(glossariesTab.getId(), glossariesTab.getLabel());
-        Main.main.appendChild(glossariesTab.getContainer());
-        Main.tabs.set(glossariesTab.getId(), glossariesTab.getContainer());
+        Main.tabHolder.addTab(glossariesTab);
 
-        Main.selectTab('projects');
+        Main.tabHolder.selectTab('projects');
 
         this.electron.ipcRenderer.send('get-theme');
         this.electron.ipcRenderer.on('set-theme', (event: Electron.IpcRendererEvent, arg: any) => {
@@ -126,16 +63,16 @@ class Main {
             this.resizePanels();
         })
         this.electron.ipcRenderer.on('view-projects', () => {
-            Main.selectTab('projects');
+            Main.tabHolder.selectTab('projects');
         });
         this.electron.ipcRenderer.on('request-projects', () => {
             this.projectsView.loadProjects();
         });
         this.electron.ipcRenderer.on('view-memories', () => {
-            Main.selectTab('memories');
+            Main.tabHolder.selectTab('memories');
         });
         this.electron.ipcRenderer.on('view-glossaries', () => {
-            Main.selectTab('glossaries');
+            Main.tabHolder.selectTab('glossaries');
         });
         this.electron.ipcRenderer.on('start-waiting', () => {
             document.getElementById('body').classList.add("wait");
@@ -164,62 +101,26 @@ class Main {
         }
     }
 
-    static selectTab(tab: string): void {
-        Main.labels.forEach(function (value, key) {
-            if (value.classList.contains('selectedTab')) {
-                value.classList.remove('selectedTab');
-            }
-        });
-        Main.labels.get(tab).classList.add('selectedTab');
-        Main.labels.get(tab).blur();
-
-        Main.tabs.forEach(function (value, key) {
-            if (!value.classList.contains('hidden')) {
-                value.classList.add('hidden');
-            }
-        });
-        Main.tabs.get(tab).classList.remove('hidden');
-    }
-
-    static closeTab(tab: string): void {
-        Main.tabHolder.removeChild(Main.labels.get(tab));
-        Main.labels.delete(tab);
-        Main.main.removeChild(Main.tabs.get(tab));
-        Main.tabs.delete(tab);
-
-        Main.selectTab('projects');
-    }
-
     addTab(arg: any) {
-        if (Main.tabs.has(arg.id)) {
-            Main.selectTab(arg.id);
+        if (Main.tabHolder.has(arg.id)) {
+            Main.tabHolder.selectTab(arg.id);
             return;
         }
         let tab = new Tab(arg.id, arg.description, true);
-
-        Main.labels.set(tab.getId(), tab.getLabel());
-        Main.tabHolder.appendChild(tab.getLabel());
-
-        Main.tabs.set(tab.getId(), tab.getContainer());
-        Main.main.appendChild(tab.getContainer());
-
         new TranslationView(tab.getContainer(), arg.id);
-
-        Main.selectTab(arg.id);
+        Main.tabHolder.addTab(tab);
+        Main.tabHolder.selectTab(arg.id);
     }
 
     buildProjectsTab(div: HTMLDivElement): void {
-        div.classList.add('hidden');
         this.projectsView = new ProjectsView(div);
     }
 
     buildMemoriesTab(div: HTMLDivElement): void {
-        div.classList.add('hidden');
         this.memoriesView = new MemoriesView(div);
     }
 
     buildGlossariesTab(div: HTMLDivElement): void {
-        div.classList.add('hidden');
         this.glossariesView = new GlossariesView(div);
     }
 
---
+++ ts/tabs.ts
@@ -0,0 +1,133 @@
+/*****************************************************************************
+Copyright (c) 2007-2020 - Maxprograms,  http://www.maxprograms.com/
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to compile, 
+modify and use the Software in its executable form without restrictions.
+
+Redistribution of this Software or parts of it in any form (source code or 
+executable binaries) requires prior written permission from Maxprograms.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+*****************************************************************************/
+
+class Tab {
+
+    id: string;
+    label: HTMLAnchorElement;
+    labelDiv: HTMLDivElement;
+    container: HTMLDivElement;
+    parent: TabHolder;
+
+    constructor(tabId: string, description: string, closeable: boolean) {
+        this.id = tabId;
+        this.labelDiv = document.createElement('div');
+        this.labelDiv.classList.add('tab');
+
+        this.label = document.createElement('a');
+        this.label.id = this.id;
+        this.label.innerText = description;
+        this.label.addEventListener('click', () => {
+            this.parent.selectTab(this.id);
+        });
+        this.labelDiv.appendChild(this.label);
+        if (closeable) {
+            let closeAnchor: HTMLAnchorElement = document.createElement('a');
+            closeAnchor.className = 'tooltip';
+            closeAnchor.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path style="stroke-width: 1.4;" d="M4 4 L12 12 M4 12 L12 4"/></svg>' +
+                '<span  class="tooltiptext bottomTooltip">Close</span>';
+            closeAnchor.style.marginLeft = '10px';
+            closeAnchor.addEventListener('click', () => {
+                this.parent.closeTab(this.id);
+            });
+            this.labelDiv.appendChild(closeAnchor);
+        }
+        this.container = document.createElement('div');
+        this.container.classList.add('hidden');
+    }
+
+    setParent(holder: TabHolder): void {
+        this.parent = holder;
+    }
+
+    getId(): string {
+        return this.id;
+    }
+
+    getLabel(): HTMLDivElement {
+        return this.labelDiv;
+    }
+
+    getContainer(): HTMLDivElement {
+        return this.container;
+    }
+
+    setContainer(div: HTMLDivElement): void {
+        this.container = div;
+    }
+}
+
+class TabHolder {
+
+    labels: Map<String, HTMLDivElement>;
+    tabs: Map<string, HTMLDivElement>;
+
+    tabsHolder: HTMLDivElement;
+    contentHolder: HTMLDivElement;
+
+    constructor(parent: HTMLDivElement, id: string) {
+        this.labels = new Map<String, HTMLDivElement>();
+        this.tabs = new Map<string, HTMLDivElement>();
+
+        this.tabsHolder = document.createElement('div');
+        this.tabsHolder.classList.add('tabHolder');
+        parent.appendChild(this.tabsHolder);
+
+        this.contentHolder = document.createElement('div');
+        this.contentHolder.id = id;
+        this.contentHolder.classList.add('fill_width');
+        parent.appendChild(this.contentHolder);
+    }
+
+    addTab(tab: Tab): void {
+        tab.setParent(this);
+        this.tabsHolder.appendChild(tab.getLabel());
+        this.labels.set(tab.getId(), tab.getLabel());
+        this.contentHolder.appendChild(tab.getContainer());
+        this.tabs.set(tab.getId(), tab.getContainer());
+    }
+
+    selectTab(tab: string): void {
+        this.labels.forEach(function (value, key) {
+            if (value.classList.contains('selectedTab')) {
+                value.classList.remove('selectedTab');
+            }
+        });
+        this.labels.get(tab).classList.add('selectedTab');
+        this.labels.get(tab).blur();
+
+        this.tabs.forEach(function (value, key) {
+            if (!value.classList.contains('hidden')) {
+                value.classList.add('hidden');
+            }
+        });
+        this.tabs.get(tab).classList.remove('hidden');
+    }
+
+    closeTab(tab: string): void {
+        this.tabsHolder.removeChild(this.labels.get(tab));
+        this.labels.delete(tab);
+        this.contentHolder.removeChild(this.tabs.get(tab));
+        this.tabs.delete(tab);
+    }
+
+    has(tab: string): boolean {
+        return this.labels.has(tab);
+    }
+}
