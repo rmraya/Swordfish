17b1a544065d55694a5c496f729e7f5e0a6b37c9
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2020-08-11 08:50:32 -0300

    code cleanup

--- src/com/maxprograms/swordfish/tm/TMUtils.java
+++ src/com/maxprograms/swordfish/tm/TMUtils.java
@@ -25,7 +25,6 @@ import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
