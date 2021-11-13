775853c9526115a8157c265b1a63ebf659a5678e
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-11-13 07:32:49 -0300

    Fixed segmentation issues with MS Office

--- src/com/maxprograms/swordfish/Constants.java
+++ src/com/maxprograms/swordfish/Constants.java
@@ -19,8 +19,8 @@ public class Constants {
 	}
 
 	public static final String APPNAME = "Swordfish";
-	public static final String VERSION = "4.11.0";
-	public static final String BUILD = "20211121018";
+	public static final String VERSION = "4.11.1";
+	public static final String BUILD = "20211113_0654";
 
 	public static final String REASON = "reason";
 	public static final String STATUS = "status";
