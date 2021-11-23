fa7e65895d08548ce56f3ecc1ebfbe32f4724328
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-11-23 14:43:59 -0300

    Improved conversion of XLIFF 2.0 to 1.2

--- src/com/maxprograms/swordfish/Constants.java
+++ src/com/maxprograms/swordfish/Constants.java
@@ -19,8 +19,8 @@ public class Constants {
 	}
 
 	public static final String APPNAME = "Swordfish";
-	public static final String VERSION = "4.12.0";
-	public static final String BUILD = "20211121_1941";
+	public static final String VERSION = "4.12.1";
+	public static final String BUILD = "20211123_1340";
 
 	public static final String REASON = "reason";
 	public static final String STATUS = "status";
