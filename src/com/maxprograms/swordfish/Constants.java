2ecfeabb8ba0ff7353ba401fd2ac48d44d33b27a
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-03-17 15:01:24 -0300

    Fixed detection of QTI files

--- src/com/maxprograms/swordfish/Constants.java
+++ src/com/maxprograms/swordfish/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
 
 	public static final String APPNAME = "Swordfish";
 	public static final String VERSION = "5.19.0";
-	public static final String BUILD = "20260316_1515";
+	public static final String BUILD = "20260317_1459";
 
 	public static final String REASON = "reason";
 	public static final String STATUS = "status";
